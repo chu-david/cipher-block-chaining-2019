d782de0edb8f25ec9be18996e6d621b0124c416f
chu-david <dchu@emmauscc.sa.edu.au>
2019-04-08 21:59:16 +0930

    skeleton

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-//Write Code Here
-$(document).ready(function(){
+$(document).ready(main);
+
+function main(){
+
     
-    var cipherText ="";
-    var block="";
 
-    var text = ReadFile("iHaveADream2.txt");
-    //var text="hello";
-
-    var current="";
-
-    var init = "11111111";
-    //var i=0;
-
-    while(text.length >0){
-        var binary = convertToBinary(text.charAt(0));       
-        var encInput = XORgate(init,binary); 
-        text = text.substring(1,text.length);
-        init = encrypt(encInput, 5,"caesar");
-        block = block+init;
-    }
-    console.log(block);
-})
+}
