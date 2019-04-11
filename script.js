fc74de5e5ce03144137b90966b5a6f85db4c288b
Matthew ROHRLACH <matthew.rohrlach@students.emmauscc.sa.edu.au>
2019-04-11 12:08:12 +0930

    input added

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 $(document).ready(main);
 
+    var projectManager = "Matthew";
+
+    var cipherText = prompt("Type the letters to be encrypted ");
+
+    var intVector = [0, 1, 0, 0, 1, 1, 0, 1];
+
 function main(){
 
-    
+    console.log(cipherText);
 
 }
