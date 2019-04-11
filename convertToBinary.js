e008f000944832dcafd6628a5ec3278ceba2954e
Armin SCHMAREWSKI <armin.schmarewski@students.emmauscc.sa.edu.au>
2019-04-11 11:33:01 +0930

    basic function setup

---
+++ convertToBinary.js
@@ -0,0 +1,12 @@
+console.log("Success");
+
+function decToBin(dec){
+
+    var binaryArray = new array(8);
+    for(i=7; i>0; i--){
+        
+        console.log(Math.pow(2,i));
+        
+    
+    }
+}
