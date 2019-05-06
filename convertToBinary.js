252a50ab4d817b2c3bb9bd1210138550d8cf7dd0
Armin SCHMAREWSKI <armin.schmarewski@students.emmauscc.sa.edu.au>
2019-05-06 13:48:27 +0930

    moved from encrypt to cinverToBinary

--- convertToBinary.js
+++ convertToBinary.js
@@ -50,3 +50,35 @@ function decToBin(decimal){
     return binaryArray;
 
 }
+
+function binaryToString(binaryDoubleArray){
+
+    var string = "";
+
+    for(var i = 0; i<binaryDoubleArray.length; i++){
+
+        string += String.fromCharCode(binToDec(binaryDoubleArray[i])); 
+
+    }
+
+    return string;
+
+}
+
+function binToDec(binary){
+
+    var decimal = 0;
+
+    for(var i = 0; i<8; i++){
+
+        if(binary[i] == 1){
+
+            decimal += Math.pow(2, 7-i);
+
+        }
+
+    }
+
+    return decimal;
+
+} 
